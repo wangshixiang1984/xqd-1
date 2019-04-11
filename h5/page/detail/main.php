4d8a2b320f00158068bacbb05213727e8a92dd81
wangshixiang1984 <1259565044@qq.com>
2019-04-11 21:14:17 +0800

    detail/index.php  详情页

---
+++ h5/page/detail/main.php
@@ -0,0 +1,189 @@
+<div class="swiper-container" id="swiper3">
+    <div class="swiper-wrapper">
+        <div class="swiper-slide">
+            <img src="http://www.xqdzjy.com/htmleditor/attached/image/mainpic/201903284418.jpg" />
+        </div>
+        <div class="swiper-slide">
+            <img src="http://www.xqdzjy.com//htmleditor/attached/image/mainpic/201903267058.jpg" />
+        </div>
+        <div class="swiper-slide">
+            <img src="http://www.xqdzjy.com/htmleditor/attached/image/mainpic/201903284418.jpg" />
+        </div>
+    </div>
+    <!-- 如果需要分页器 -->
+    <div class="swiper-pagination"></div>
+</div>
+<div class="p10 bg-w xc-wp">
+    <div class="dt-head brd-btm-das">
+        <h1 class="mt10">标题【世外桃源】广西桂林8日环线自驾游</h1>
+        <div class="row-lr">
+            <ul class="col-l lm">
+                <li><i class="iconfont icondizhi"></i>出发城市：成都</li>
+                <li><i class="iconfont iconqing"></i>行程里程：20521km</li>
+                <li><i class="iconfont iconshengqian"></i>行程天数：21天</li>
+                <li><i class="iconfont iconqian"></i>1180.00/人起</li>
+                <li><i class="iconfont icondianhua3"></i>报名热线：<div class="pl15">028-83918255</div></li>
+                <li>
+                    <div id="date"><i class="iconfont iconriqi"></i>发团日期<i class="iconfont iconxiala fr"></i></div>
+                    <div class="lis" id="list">
+                        <p >2019-04-05  <span class="red"><span class="red">¥</span></span>1180/成人  <span class="red">¥</span>900/儿童（余20位）</p>
+                        <p >2019-04-05  <span class="red">¥</span>1180/成人  <span class="red">¥</span>900/儿童（余20位）</p>
+                        <p >2019-04-05  <span class="red">¥</span>1180/成人  <span class="red">¥</span>900/儿童（余20位）</p>
+                        <p >2019-04-05  <span class="red">¥</span>1180/成人  <span class="red">¥</span>900/儿童（余20位）</p>
+                        <p >2019-04-05  <span class="red">¥</span>1180/成人  <span class="red">¥</span>900/儿童（余20位）</p>
+                        <p >2019-04-05  <span class="red">¥</span>1180/成人  <span class="red">¥</span>900/儿童（余20位）</p>
+                        <p >2019-04-05  <span class="red">¥</span>1180/成人  <span class="red">¥</span>900/儿童（余20位）</p>
+                        <p >2019-04-05  <span class="red">¥</span>1180/成人  <span class="red">¥</span>900/儿童（余20位）</p>
+                        <p >2019-04-05  <span class="red">¥</span>1180/成人  <span class="red">¥</span>900/儿童（余20位）</p>
+                        <p >2019-04-05  <span class="red">¥</span>1180/成人  <span class="red">¥</span>900/儿童（余20位）</p>
+                        <p >2019-04-05  <span class="red">¥</span>1180/成人  <span class="red">¥</span>900/儿童（余20位）</p>
+                        <p >2019-04-05  <span class="red">¥</span>1180/成人  <span class="red">¥</span>900/儿童（余20位）</p>
+                    </div>
+                </li>
+            </ul>
+            <div class="col-r rt p10 f11-ellipsis">桂林是世界著名的旅游城市、中国首批国家历史文化名城、中国优秀旅游城市，其境内的山水风光举世闻名，千百年来享有"桂林山水甲天下"的美誉。
+    桂林地处广西壮族自治区东北部，是广西最大空港，桂东北地区的政治、经济、文化、交通中心，桂林北接湖南、贵州，西面、城，洞奇石美的独特景观。</div>
+        </div>
+    </div>
+    <div class="brd-btm-das ptb20">
+        <p class="tit">行程亮点</p>
+        <div class="f3-ellipsis mt10" id="des">
+            1. 牟尼沟：牟尼沟位于四川省阿坝藏族羌族自治州松潘县境内，2011年顶峰国际旅游规划项目。距成都三百多公里，占地160平方公里，最高海拔4070米，最低海拔2800米，年平均气温7℃，自然风光迷人、民族风情浓郁、汇聚了原始森林之神韵，牟尼沟风景主要以原始森林、高山湖泊、温泉、巨型钙化瀑布、环形瀑布、钙化彩池为主要特色，景区由扎嘎瀑布和二道海两部分组成。山、水、林、在这里巧妙地融为一体。主要景点有:扎嘎瀑布、牟尼森林、百亩杜鹃、翡翠温泉、百花湖、月亮湖、天鹅湖和溶洞群等。
+        </div>
+        <div class="btn btn-line purple mtb10">阅读更多<i class="iconfont iconxiala"></i></div>
+    </div>
+    <div class="ptb20 dt-xc">
+        <p class="tit">行程安排</p>
+        <div class="mod mb10">
+            <p class="day">第一天</p>
+            <p class="js mtb10"><i class=" mr10"><img src="/h5/public/images/js.png" /></i>行程介绍：成都-黄果树 700km</p>
+            <div class="mt10">
+                <p class="mb10">早上从指定地点集合，张贴车标，分发对讲机完毕从成都出发至黄果树，12:00 服务区中餐（自理）后继续出发前往，为了接下来7天的精彩今天全天都在路上，小小辛苦美景当前还是值得的哈！ 19:00 到达，入住黄果树维也纳酒店后，自由活动。</p>
+                <div class="pic"><img src="/htmleditor/attached/image/mainpic/201903202685.jpg" /></div>
+                <div class="pic"><img src="/htmleditor/attached/image/mainpic/201903202685.jpg" /></div>
+            </div>
+            <p class="js mt10"><i class="mr10"><img src="/h5/public/images/yc.png" /></i>用餐：午餐 晚餐</p>
+            <p class="js"><i class="mr10"><img src="/h5/public/images/zs.png" /></i>住宿：黄果树大酒店</p>
+        </div>
+        <div class="mod mb10">
+            <p class="day">第二天</p>
+            <p class="js mtb10"><i class=" mr10"><img src="/h5/public/images/js.png" /></i>行程介绍：成都-黄果树 700km</p>
+            <div class="mt10">
+                <p class="mb10">早上从指定地点集合，张贴车标，分发对讲机完毕从成都出发至黄果树，12:00 服务区中餐（自理）后继续出发前往，为了接下来7天的精彩今天全天都在路上，小小辛苦美景当前还是值得的哈！ 19:00 到达，入住黄果树维也纳酒店后，自由活动。</p>
+                <div class="pic"><img src="/htmleditor/attached/image/mainpic/201903202685.jpg" /></div>
+                <div class="pic"><img src="/htmleditor/attached/image/mainpic/201903202685.jpg" /></div>
+            </div>
+            <p class="js mt10"><i class="mr10"><img src="/h5/public/images/yc.png" /></i>用餐：午餐 晚餐</p>
+            <p class="js"><i class="mr10"><img src="/h5/public/images/zs.png" /></i>住宿：黄果树大酒店</p>
+        </div>
+        <div class="mod mb10">
+            <p class="day">第三天</p>
+            <p class="js mtb10"><i class=" mr10"><img src="/h5/public/images/js.png" /></i>行程介绍：成都-黄果树 700km</p>
+            <div class="mt10">
+                <p class="mb10">早上从指定地点集合，张贴车标，分发对讲机完毕从成都出发至黄果树，12:00 服务区中餐（自理）后继续出发前往，为了接下来7天的精彩今天全天都在路上，小小辛苦美景当前还是值得的哈！ 19:00 到达，入住黄果树维也纳酒店后，自由活动。</p>
+                <div class="pic"><img src="/htmleditor/attached/image/mainpic/201903202685.jpg" /></div>
+                <div class="pic"><img src="/htmleditor/attached/image/mainpic/201903202685.jpg" /></div>
+            </div>
+            <p class="js mt10"><i class="mr10"><img src="/h5/public/images/yc.png" /></i>用餐：午餐 晚餐</p>
+            <p class="js"><i class="mr10"><img src="/h5/public/images/zs.png" /></i>住宿：黄果树大酒店</p>
+        </div>
+    </div>
+    <div class="dt-fy">
+        <p class="tit">费用说明</p>
+        <div class="con mt10">
+            成人价  3280 元/人，儿童价（身高1.2m以下） 1148  元/人(只含餐保、服务费)，建议拼车价1060 元/人，单房差 877 元/人，（酒店为双人标间，每人只含一张床位，但本团队出现单男单女和要求单住的客人，请另补单房差）。
+            <p> 住宿：全程高级标间；
+            用餐：7个早餐；
+            门票：行程中所涉及景点门票（所有景点只含第一景区大门票）
+            领队：全程资深领队带队、生活领队；
+            保险：专业自驾车保险（最高保险金额80万）；
+            通讯：每车配备1台对讲机使用（如遗失或损坏需赔偿400元）以及220V车载电源一个（如遗失或损坏需赔偿100元）。
+            专业硬牌越野车、专业汽修配套、专业摄影师、车辆故障解码器</p>
+            <i></i>
+        </div>
+    </div>
+    <div class="dt-fy mt20 sx brd-btm-das">
+        <p class="tit">注意事项</p>
+        <div class="con mt10">
+            成人价  3280 元/人，儿童价（身高1.2m以下） 1148  元/人(只含餐保、服务费)，建议拼车价1060 元/人，单房差 877 元/人，（酒店为双人标间，每人只含一张床位，但本团队出现单男单女和要求单住的客人，请另补单房差）。
+            <p> 住宿：全程高级标间；
+            用餐：7个早餐；
+            门票：行程中所涉及景点门票（所有景点只含第一景区大门票）
+            领队：全程资深领队带队、生活领队；
+            保险：专业自驾车保险（最高保险金额80万）；
+            通讯：每车配备1台对讲机使用（如遗失或损坏需赔偿400元）以及220V车载电源一个（如遗失或损坏需赔偿100元）。
+            专业硬牌越野车、专业汽修配套、专业摄影师、车辆故障解码器</p>
+        </div>
+    </div>
+    <div class="m-lay">
+        <p class="f16 mtb10">你可能也喜欢</p>
+        <ul class="cate like">
+            <li>
+                <div class="m-item">
+                    <img src="http://www.xqdzjy.com/htmleditor/attached/image/mainpic/201608188674.jpg" />
+                </div>
+                <p class="mt5">这个地方是标题</p>
+                <p class="f12">￥1180.00/人起</p>
+            </li>
+            <li>
+                <div class="m-item">
+                    <img src="http://www.xqdzjy.com/htmleditor/attached/image/mainpic/201608188674.jpg" />
+                </div>
+                <p class="mt5">这个地方是标题</p>
+                <p class="f12">￥1180.00/人起</p>
+            </li>
+            <li>
+                <div class="m-item">
+                    <img src="http://www.xqdzjy.com/htmleditor/attached/image/mainpic/201608188674.jpg" />
+                </div>
+                <p class="mt5">这个地方是标题</p>
+                <p class="f12">￥1180.00/人起</p>
+            </li>
+            <li>
+                <div class="m-item">
+                    <img src="http://www.xqdzjy.com/htmleditor/attached/image/mainpic/201608188674.jpg" />
+                </div>
+                <p class="mt5">这个地方是标题</p>
+                <p class="f12">￥1180.00/人起</p>
+            </li>
+        </ul>
+    </div>
+</div>
+
+
+<script src="/h5/public/js/swiper.js"></script>
+<script>
+var mySwiper3 = new Swiper ('#swiper3', {
+        loop: true, // 循环模式选项
+        // 如果需要分页器
+        pagination: {
+        el: '.swiper-pagination',
+        bulletActiveClass: 'index-bullet-active',
+        },
+
+    })  
+
+$(function() {
+    // 阅读更多 收起
+    $('.purple').click(function(e){
+        var des = $("#des");
+        if(des.hasClass('f3-ellipsis')) {
+            $(this).html('收起<i class="iconfont iconshouqi"></i>');
+            $("#des").removeClass('f3-ellipsis');
+        } else {
+            $(this).html('阅读更多<i class="iconfont iconxiala"></i>');
+            $("#des").addClass('f3-ellipsis');
+        }
+    })
+    // 发团日期下拉
+    $('#date').click(function(){
+        $('#list').toggle();
+    })
+    $('body').click(function(e) {
+        e.stopPropagation();
+        if($(e.target).attr('id') != 'date') {
+            $('#list').hide();
+        }
+    })
+})      
+</script>
